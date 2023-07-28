475b48004343fc4359647930d049ef4c896c1e7d
edricus <edricus@protonmail.com>
2023-07-28 14:47:07 +0200

    create public lb

--- main.tf
+++ main.tf
@@ -45,4 +45,26 @@ resource "azurerm_subnet" "subnet-4" {
   virtual_network_name = azurerm_virtual_network.vnet-1.name
   address_prefixes     = ["10.0.0.48/28"]
 }
+resource "azurerm_public_ip" "publiclb-ip" {
+  name                = "publiclb-ip"
+  resource_group_name = var.rg
+  location            = var.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "publiclb" {
+  name                = "publiclb"
+  resource_group_name = var.rg
+  location            = var.location
+
+  frontend_ip_configuration {
+    name                 = "publiclb-ip-association"
+    public_ip_address_id = azurerm_public_ip.publiclb-ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "publiclb-backend" {
+loadbalancer_id = azurerm_lb.publiclb.id
+name            = "publiclb-backend"
+}
 
